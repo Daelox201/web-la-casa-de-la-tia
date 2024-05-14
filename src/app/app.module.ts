3274faf301e4e383021054c4740d6a2157541727
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-05-13 23:06:09 -0600

    Creacion de componenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    MenuComponent,
+    QuienesSomosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'inicio', component:InicioComponent},
+      {path:'menu', component:MenuComponent},
+      {path:'quienes-somos', component:QuienesSomosComponent},
+      {path:'contacto', component:ContactoComponent},
+      {path: '**', redirectTo:'/', pathMatch:'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
